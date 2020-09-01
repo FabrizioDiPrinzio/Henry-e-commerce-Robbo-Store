a00439a47c943a69813cbb91a399372bcb12004a
Lismairy Sanchez <sanchezlismairy@gmail.com>
2020-09-01 14:49:46 -0300

    Cambio de puerto 3001

--- api/src/db.js
+++ api/src/db.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config(); //Es la forma de requerir el archivo .env//
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');

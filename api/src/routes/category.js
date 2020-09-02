b6b0978e262a0399ce7c8e3fcda2796f9af456f7
Agustin Jaralampidis <ajaralampidis@gmail.com>
2020-09-02 19:24:15 -0300

    category create edit delete routes done

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {Categories} = require('../db.js');
 
-/*
-POST /products/category/
-Crea una categoría nueva.
-
-Story 07
-*/
 router.post('/', (req, res) => {
 	const {name, description} = req.body;
 	if (!name || !description)
@@ -16,10 +10,36 @@ router.post('/', (req, res) => {
 			.catch(err => res.status(400).send(err.message));
 });
 
-/*
-DELETE /products/category/:id
 
-Story 07
-*/
+router.delete('/:id', (req, res) => {
+	const {id} = req.params;
+
+	Categories.destroy({where: {id}}).then(response => {
+		if (response === 0) return res.sendStatus(404);
+		else return res.sendStatus(200);
+	});
+})
+
+
+router.put('/:id', (req, res) => {
+	const {name, description} = req.body;
+	const {id} = req.params;
+
+	if (!name || !description) return res.sendStatus(400);
+	else {
+		Categories.findByPk(id)
+			.then(category => {
+				if (!category) return res.status(400).send('No se encontró la categoria :(');
+
+				category.name = name ? name : category.name;
+				category.description = description ? description : category.desciription;
+				category.save();
+
+				return res.send(category);
+			})
+			.catch(err => res.status(400).send(err.message));
+	}
+});
+
 
 module.exports = router;

dbf65f9098e6bedcc6679ae64c6b11a5ef029388
Luciobio <pardobio@gmail.com>
2020-09-08 01:04:21 -0300

    product CSS

--- client/src/Components/Producto/Producto.jsx
+++ client/src/Components/Producto/Producto.jsx
@@ -21,17 +21,21 @@ export default function Producto() {
 		return <h1>Lo sentimos, pero ese producto no se encuentra en nuestra base de datos!</h1>;
 
 	return (
-		<div className="Container">
-			<div className="">
-				<h3 className="">{robot.name}</h3>
-				<img src={robot.image} alt={robot.name} />
-				<p className="">{robot.description}</p>
-				<ul className="">
-					<li className="">U$S{robot.price}</li>
-					<li className="">
-						{robot.stock > 0 ? `Stock: ${robot.stock} unidades` : 'Out of stock!'}
-					</li>
-				</ul>
+		<div className="productContainer">
+			<div className="productCont2">
+				<h3 className="productTitle">{robot.name}</h3>
+				<div>
+					<img className="productImg" src={robot.image} alt={robot.name} />
+				</div>
+				<div className="infoCard">
+					<p className="infoCardDesc">{robot.description}</p>
+					<ul className="infoCardData">
+						<li className="">U$S{robot.price}</li>
+						<li className="">
+							{robot.stock > 0 ? `Stock: ${robot.stock} unidades` : 'Out of stock!'}
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);

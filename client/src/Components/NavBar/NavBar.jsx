1491f762aeb8f2584324942526dd865c4a831cc3
Agustin Jaralampidis <ajaralampidis@gmail.com>
2020-09-09 12:43:03 -0300

    User Form is now implemented in front

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -29,21 +29,23 @@ export default function NavBar() {
 				<img src="../favicon.svg" alt="logo" className="logo" />
 				<span className="espacioBlanco"> </span>
 				<div>
-					<button className="UserBtn">
-						<svg
-							width="2em"
-							height="2em"
-							viewBox="0 0 16 16"
-							className="bi bi-person"
-							fill="white"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fillRule="evenodd"
-								d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
-							/>
-						</svg>
-					</button>
+          <Link to="/userRegistration">
+            <button className="UserBtn">
+              <svg
+                width="2em"
+                height="2em"
+                viewBox="0 0 16 16"
+                className="bi bi-person"
+                fill="white"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+                />
+              </svg>
+            </button>
+          </Link>
 
 					<button className="CartBtn">
 						<svg

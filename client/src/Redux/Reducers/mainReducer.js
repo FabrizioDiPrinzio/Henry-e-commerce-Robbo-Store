76e4fd30fab7a48c798b61d2f2e5ba4edb53514b
Fabrizio Di Prinzio <67496905+FabrizioDiPrinzio@users.noreply.github.com>
2020-09-09 00:18:27 -0300

    Product Forms ahora funciona con Redux

--- client/src/Redux/Reducers/mainReducer.js
+++ client/src/Redux/Reducers/mainReducer.js
@@ -1,11 +1,11 @@
 import {combineReducers} from 'redux';
 import categoryReducer from './categoryReducer';
-// import productReducer from './productReducer';
+import productReducer from './productReducer';
 // import userReducer from './userReducer';
 
 export default combineReducers({
-	categories: categoryReducer
+	categories: categoryReducer,
+	products: productReducer
 	// carrito: carritoReducer
-	// products: productReducer,
 	// users: userReducer
 });

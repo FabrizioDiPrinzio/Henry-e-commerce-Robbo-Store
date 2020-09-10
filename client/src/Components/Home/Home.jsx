3b3c4b72fef826126a506dcac01c5dd363c75bc3
Fabrizio Di Prinzio <67496905+FabrizioDiPrinzio@users.noreply.github.com>
2020-09-10 13:02:51 -0300

    Esqueleto Purchase_Order_Form

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Carousel from './Carousel.js';
 import './Home.css';
 import {Link} from 'react-router-dom';
 
@@ -9,13 +8,9 @@ function Home() {
 			<div className="Carousel">
 				<h1>[ Carousel ]</h1>
 			</div>
-			<Link to="/product_form">
-				<h3>Product Form</h3>
+			<Link to="/admin">
+				<h3>Admin control panel</h3>
 			</Link>
-			<Link to="/category_form">
-				<h3>Category Form</h3>
-			</Link>
-
 		</div>
 	);
 }
--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -29,41 +29,40 @@ export default function NavBar() {
 				<img src="../favicon.svg" alt="logo" className="logo" />
 				<span className="espacioBlanco"> </span>
 				<div>
-          
-          <Link to="/signup">
-            <button className="UserBtn">
-              <svg
-                width="2em"
-                height="2em"
-                viewBox="0 0 16 16"
-                className="bi bi-person"
-                fill="white"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
-                />
-              </svg>
-            </button>
-          </Link>
-        
-					<Link to='/carrito'>
-					<button className="CartBtn">
-						<svg
-							width="2em"
-							height="2em"
-							viewBox="0 0 16 16"
-							className="bi bi-cart2"
-							fill="white"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fillRule="evenodd"
-								d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"
-							/>
-						</svg>
-					</button>
+					<Link to="/signup">
+						<button className="UserBtn">
+							<svg
+								width="2em"
+								height="2em"
+								viewBox="0 0 16 16"
+								className="bi bi-person"
+								fill="white"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M10 5a2 2 0 1 1-4 0 2 2 0 0 1 4 0zM8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm6 5c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+								/>
+							</svg>
+						</button>
+					</Link>
+
+					<Link to="/carrito">
+						<button className="CartBtn">
+							<svg
+								width="2em"
+								height="2em"
+								viewBox="0 0 16 16"
+								className="bi bi-cart2"
+								fill="white"
+								xmlns="http://www.w3.org/2000/svg"
+							>
+								<path
+									fillRule="evenodd"
+									d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"
+								/>
+							</svg>
+						</button>
 					</Link>
 				</div>
 			</nav>

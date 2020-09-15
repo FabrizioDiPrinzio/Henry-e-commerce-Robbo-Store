fcda12f8ae60964f536c0d807073818abb8577a6
Fabrizio Di Prinzio <67496905+FabrizioDiPrinzio@users.noreply.github.com>
2020-09-15 00:02:08 -0300

    Cree pagina 404 y oculte rutas de admin

---
+++ client/src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.css';
+
+export default function NotFound() {
+	return (
+		<div className="not-found">
+			<h1>404 no encontrado</h1>
+			<p>(Añadir estilo más tarde)</p>
+		</div>
+	);
+}

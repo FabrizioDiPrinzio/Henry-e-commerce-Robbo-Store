268e7c717850cc6a9f37cf364bd820eea0ac47ca
Luciobio <pardobio@gmail.com>
2020-09-17 03:58:39 -0300

    Product.js carousel

--- client/src/Components/Producto/Producto.jsx
+++ client/src/Components/Producto/Producto.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
 import './Producto.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel'
 import Axios from 'axios';
 // =========== FIN DE IMPORTS ============
 
@@ -9,11 +10,16 @@ const urlBack = process.env.REACT_APP_API_URL;
 
 export default function Producto() {
 	const [robot, setRobot] = useState({});
+	const [pics, setPics] = useState([]);
+	const [index, setIndex] = useState(0);
 	const {id} = useParams();
 
 	useEffect(
 		() => {
-			Axios.get(`${urlBack}/products/${id}`).then(res => setRobot(res.data));
+			Axios.get(`${urlBack}/products/${id}`).then(res => {
+				setRobot(res.data);
+				setPics(res.data.pics.map(i => i.imageUrl));
+			});
 		},
 		[id]
 	);
@@ -25,17 +31,33 @@ export default function Producto() {
 			</h1>
 		);
 
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
 	return (
 		<div className="productContainer">
 			<div className="productCont2">
 				<h3 className="productTitle">{robot.name}</h3>
-				<div>
-					<img className="productImg" src={robot.image} alt={robot.name} />
-				</div>
+					<Carousel className="carItems" activeIndex={index} onSelect={handleSelect}>
+						{pics.map(image => (
+							<Carousel.Item className="">
+								<img
+								//d-block w-100
+									className="productImg w-100"
+									src={image}
+									alt={pics.indexOf(image)}
+								/>
+								<Carousel.Caption className="carCap">
+									<h4></h4>
+								</Carousel.Caption>
+							</Carousel.Item>
+						))}
+					</Carousel>
 				<div className="infoCard">
 					<p className="infoCardDesc">{robot.description}</p>
 					<ul className="infoCardData">
-						<li className="">U$S{robot.price}</li>
+						<li className="">$ {robot.price}</li>
 						<li className="">
 							{robot.stock > 0 ? `Stock: ${robot.stock} unidades` : 'Out of stock!'}
 						</li>
@@ -44,4 +66,4 @@ export default function Producto() {
 			</div>
 		</div>
 	);
-}
+};

7b1ba9b8f9984c598546accb36303c007065c74c
Luciobio <pardobio@gmail.com>
2020-09-25 05:36:29 -0300

    Control Panel Filter (maso)

--- client/src/Components/AdminControlPanel/PurchaseOrderForm/PurchaseOrderForm.jsx
+++ client/src/Components/AdminControlPanel/PurchaseOrderForm/PurchaseOrderForm.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 import './PurchaseOrderForm.css';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -7,23 +7,51 @@ import PurchaseOrderFormLine from './PurchaseOrderFormLine/PurchaseOrderFormLine
 const urlBack = process.env.REACT_APP_API_URL;
 
 export default function PurchaseOrderForm() {
-	// const userList = useSelector(state => state.users)
-	// const dispatch = useDispatch()
 
+	const [selectedStatus, setSelectedStatus] = useState('0');
 	const [productOrders, setProductOrders] = useState([]);
+	const [orders, setOrders] = useState([]);
+	const lista = useRef(0);
 
 	useEffect(() => {
 		axios
 			.get(`${urlBack}/orders`)
 			.then(response => {
 				setProductOrders(response.data);
-				console.log(response)
+				setOrders(response.data);
 			})
 			.catch(err => console.log(err.response.data));
 	}, []);
 
+	const handleSelectChange = event => {
+		event.preventDefault();
+		setProductOrders(orders);
+
+		let status = event.target.value;
+		setSelectedStatus(status);
+
+		if (status !== "0") {
+			setProductOrders(productOrders.filter(order => order.status === status));
+		}
+		else {
+			setProductOrders(orders);
+			setSelectedStatus('0');
+		}
+	};
+
 	return (
 		<div className="productFormAdmin">
+			<div>
+				<div>Filtrar por estado: </div>
+				<select ref={lista} className="statusSelect" defaultValue="Estado..." onChange={handleSelectChange}>
+					<option selected value="0">Todo</option>
+					<option value="enCarrito">En carrito</option>
+					<option value="creada">Creada</option>
+					<option value="pagada">Pagada</option>
+					<option value="Entregada">Entregada</option>
+					<option value="Rechazada">Rechazada</option>
+				</select>
+			</div>
 			<div className="productTableTitleContainer">
 				<div className="productTableTitle">
 					<h6>Id Orden</h6>

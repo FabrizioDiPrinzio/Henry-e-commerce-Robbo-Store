36f83500b5f4a9fbf5a49485c35349bef0d607dd
Eme <bramasca@gmail.com>
2020-09-25 02:16:08 -0300

    factorización ruta producto paginado

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -19,18 +19,9 @@ router.get('/pag/', (req, res, next) => {
 	Product.findAll({include: [Categories, Pics]}).then(data => {
 		const productos = data.slice(firstIndex, lastIndex);
 		const result = {
-			prevPage: parseInt(p) < 2 ? null : parseInt(p) - 1,
-			currentPage: parseInt(p),
-			nextPage: parseInt(p) + 1
+			data : productos,
+			currentPage: productos.length > 0 ? p : p - 1
 		};
-		result.data = productos;
-		if (result.data.length < 1) {
-			result = {
-				prevPage: result.prevPage - 1,
-				currentPage: result.currentPage - 1,
-				nextPage: result.nextPage - 1
-			};
-		}
 		res.send(result);
 	});
 });

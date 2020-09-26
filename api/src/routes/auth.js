09c55af4e95920cfbb93c9bb2439996f72f71b01
Fabrizio Di Prinzio <67496905+FabrizioDiPrinzio@users.noreply.github.com>
2020-09-26 15:42:35 -0300

    Moved mailgun credentials to .env

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,13 +1,12 @@
+require('dotenv').config(); //Es la forma de requerir el archivo .env//
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const {User} = require('../db');
 
 //Mailgun
-var  apiKey  = '1f329c28bd5d533b74a297f71b1d5ee0-cb3791c4-3c5c3c83'; 
-var  domain  = 'sandbox0c3cd1b64bf049a0b1fc4ecac8fc8aae.mailgun.org'; 
-var mailgun = require ('mailgun-js') ({apiKey : apiKey , domain : domain}); 
-
+const {mailgunApiKey, mailgunDomain} = process.env;
+var mailgun = require('mailgun-js')({apiKey: mailgunApiKey, domain: mailgunDomain});
 
 // Google login
 router.get(
@@ -62,40 +61,33 @@ router.post('/forgot', async (req, res) => {
 		const user = await User.findOne({where: {email}});
 		if (!user) return res.status(404).send('No hay usuarios registrados con ese email');
 
-
-
-
 		const salt = await User.generateSalt();
 
 		user.forgotPasswordToken = salt;
 		await user.save();
 
-		
 		setTimeout(() => {
 			user.forgotPasswordToken = null;
 			user.save();
 		}, 3600000); // Caduca en una hora
 
-		const  data  = {
-			from : 'RobboStore <sanchezlismairy@gmail.com>', 
-			to : email, 
-			subject : 'Reseteo de Password', 
-			text :`http://localhost:3000/reset/${salt}`, 
-			template: "password"
-	};
+		const data = {
+			from: 'RobboStore <sanchezlismairy@gmail.com>',
+			to: email,
+			subject: 'Reseteo de Password',
+			text: `http://localhost:3000/reset/${salt}`,
+			template: 'password'
+		};
 
-		mailgun.messages().send(data, function (error, body) {
+		mailgun.messages().send(data, function(error, body) {
 			if (error) {
-					console.log({ error })
-					return res.status(200).send({salt, statusEmail: 'error'});
+				console.log({error});
+				return res.status(200).send({salt, statusEmail: 'error'});
 			}
 
-		return res.status(200).send({salt, statusEmail: 'enviado'});
-	
-	});
-
+			return res.status(200).send({salt, statusEmail: 'enviado'});
+		});
 	} catch (error) {
-		
 		return res.sendStatus(500);
 	}
 });

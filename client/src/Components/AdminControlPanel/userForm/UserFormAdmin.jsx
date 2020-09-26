8ca0eefe1f609f606facf48e14ddd005d3a1afcc
Luciobio <pardobio@gmail.com>
2020-09-26 05:45:19 -0300

    CP Forms Styling

--- client/src/Components/AdminControlPanel/userForm/UserFormAdmin.jsx
+++ client/src/Components/AdminControlPanel/userForm/UserFormAdmin.jsx
@@ -3,6 +3,7 @@ import UserFormLine from './userFormLine/UserFormLine.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './userFormAdmin.css';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 // ------ Fin de imports -----------
 
 const urlBack = process.env.REACT_APP_API_URL;
@@ -26,7 +27,10 @@ export default function UserFormAdmin() {
 
 	return (
 		<div className="userFormAdmin">
-			<h2>Usuarios</h2>
+			<Link className="goBackBtn" to="/admin/">
+				<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.427 3.021h-7.427v-3.021l-6 5.39 6 5.61v-3h7.427c3.071 0 5.561 2.356 5.561 5.427 0 3.071-2.489 5.573-5.561 5.573h-7.427v5h7.427c5.84 0 10.573-4.734 10.573-10.573s-4.733-10.406-10.573-10.406z"/></svg>
+			</Link>
+			<h3>Usuarios</h3>
 			<div className="userTableTitleContainer">
 				<div className="userTableTitle">
 					<div className="tableTitle"><h5>Nombre</h5></div>
--- client/src/Components/FormularioProducto/ProductForm.jsx
+++ client/src/Components/FormularioProducto/ProductForm.jsx
@@ -4,6 +4,7 @@ import {Table} from 'react-bootstrap';
 import {success, failure} from '../../multimedia/SVGs';
 import './ProductForm.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 //------ Fin de imports -----
 
@@ -256,6 +257,9 @@ export default function ProductFormFunction({preSelected}) {
 	return (
 		<div>
 			<form className="prodForm">
+				<Link className="goBackBtn" to="/admin/">
+					<svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.427 3.021h-7.427v-3.021l-6 5.39 6 5.61v-3h7.427c3.071 0 5.561 2.356 5.561 5.427 0 3.071-2.489 5.573-5.561 5.573h-7.427v5h7.427c5.84 0 10.573-4.734 10.573-10.573s-4.733-10.406-10.573-10.406z"/></svg>
+				</Link>
 				<h3 className="titulo">Agregar producto</h3>
 				<div className="">
 					<div className="inpt">

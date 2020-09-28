0b1eb14e37f48b24bcdf4116b63e1ca2d857ec0b
Fabrizio Di Prinzio <67496905+FabrizioDiPrinzio@users.noreply.github.com>
2020-09-28 03:19:02 -0300

    Made OAuth slightly prettier

--- client/src/Components/Loading/LoadingScreen.jsx
+++ client/src/Components/Loading/LoadingScreen.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {useLocation} from 'react-router-dom';
 import Spinner from 'react-bootstrap/Spinner';
 import logo from '../../multimedia/logo.svg';
 import './LoadingScreen.css';
 
 export default function LoadingScreen({loading}) {
+	const location = useLocation();
+
+	// So an ugly loading screen doesn't show up when the user is logging in through a third party
+	if (location.pathname === '/oauth/success') return <div />;
+
 	return (
 		<div className={`loading-screen ${loading ? 'dummy-class' : 'hideLoading'} `}>
 			<img src={logo} alt="Robbo" id="loading-logo" />
